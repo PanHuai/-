67e15cf152b3ed18753731bf6fe59fa48e7d4641
154_100006871673_panhuai <248187023@qq.com>
2018-12-13 22:33:58 +0800

    配置文件添加

---
+++ src/main/java/com/lyl/controller/LoginController.java
@@ -0,0 +1,8 @@
+package com.lyl.controller;
+
+/**
+ * Created by 潘淮  on 2018/12/13.<br>
+ */
+
+public class LoginController {
+}
---
+++ src/main/java/com/lyl/model/User.java
@@ -0,0 +1,24 @@
+package com.lyl.model;
+
+import java.time.LocalDateTime;
+
+/**
+ * Created by 潘淮  on 2018/12/13.<br>
+ */
+public class User {
+
+    private int id;
+
+    private String userName;
+
+    private String tel;
+
+    private String openId;
+
+    private String unionId;
+
+    private LocalDateTime createTime;
+
+    private int version;
+
+}
